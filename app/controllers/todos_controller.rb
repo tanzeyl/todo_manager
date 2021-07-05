182dba3ec6e8d635c3d4de04f1e6d3cd4b4a3745
Tanzeel Khan <ktanzeel80@gmail.com>
2021-07-05 19:02:44 +0530

    Added PostgreSQL

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
   def index
-    render plain: "Hello, this is /todos!"
+    render plain: "Hello, this is /todos! #{DateTime.now.to_s(:short)}"
   end
 end
