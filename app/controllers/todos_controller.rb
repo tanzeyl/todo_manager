3cb028704eb06f1edcc3c158a22b693087673859
Tanzeel Khan <ktanzeel80@gmail.com>
2021-07-05 20:39:47 +0530

    Added a new route to create new ToDos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+
   def index
     render plain: Todo.order(:due_date).map {|todo| todo.to_pleasant_string }.join("\n")
   end
@@ -7,5 +8,13 @@ class TodosController < ApplicationController
     id = params[:id]
     todo = Todo.find(id)
     render plain: todo.to_pleasant_string
+  end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
+    response_text = "Hey, your new ToDo is created with ID: #{new_todo.id}."
+    render plain: response_text
   end
 end
