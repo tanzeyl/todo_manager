caa73d2e1bff7b0477f1c60a5f74d98e1eced6ff
Tanzeel Khan <ktanzeel80@gmail.com>
2021-07-08 22:18:54 +0530

    Validate todos before saving

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,7 @@
 class Todo < ApplicationRecord
+  validates :todo_text, presence: true
+  validates :due_date, presence: true
+  validates :todo_text, length: { minimum: 2 }
   belongs_to :user
 
   def to_pleasant_string
